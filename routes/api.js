ab4feae5531a709c5a7097b17dd39adff09ed4ec
Asep Irfan Setiawan <asepirfans.9c@gmail.com>
2025-05-18 19:59:00 +0700

    update backend

--- routes/api.js
+++ routes/api.js
@@ -13,7 +13,7 @@ router.post("/login", async (req, res) => {
   const { username, roleMode } = req.body;
 
   try {
-    const response = await axios.get("https://script.google.com/macros/s/AKfycbzjrSB5d2fXQqzZ4zEXO3DBcebV4bIrpjTAu7YOySXRzkKWzLs5Co5iv1tTvmtAnkmF/exec");
+    const response = await axios.get(process.env.LOGIN_SCRIPT_URL);
     const { data } = response.data;
 
     const user = data.find((entry) => entry.Email === username);
@@ -37,12 +37,8 @@ router.post("/konfirmasi", async (req, res) => {
   const { id, kaLab, ppcAE } = req.body;
 
   try {
-    const requestBody = {
-      id: id,
-      kaLab: kaLab,
-      ppcAE: ppcAE,
-    };
-    const response = await axios.post("https://script.google.com/macros/s/AKfycby-CsqtaPza5rlpxO8Bpcem4peBxv19AmqjJMMZHKR3E0rFN2aumKexSqjT9PwLRm99/exec", requestBody);
+    const requestBody = { id, kaLab, ppcAE };
+    const response = await axios.post(process.env.KONFIRMASI_SCRIPT_URL, requestBody);
 
     if (response.status === 200 && response.data.success) {
       res.status(200).json({ success: true, message: "Konfirmasi berhasil" });
@@ -56,10 +52,8 @@ router.post("/konfirmasi", async (req, res) => {
 });
 
 router.post("/tolak", async (req, res) => {
-  const url = "https://script.google.com/macros/s/AKfycby9RmZT9RrWcyR9V54nvykHUwEEMnr-Y7Ld3c0SZX7QOu2xbW1ihzUiRxRuhsp9Q_CB/exec";
-
   try {
-    const response = await fetch(url, {
+    const response = await fetch(process.env.TOLAK_SCRIPT_URL, {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
